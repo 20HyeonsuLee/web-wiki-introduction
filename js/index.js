e532e9e5df6c979454eb28a3bfea1536681ac791
HyeonsuLee <leehyeonsu4888@naver.com>
2025-02-27 21:18:20 +0900

    위키 구현

--- js/index.js
+++ js/index.js
@@ -17,3 +17,46 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const commentForm = document.getElementById('content-form');
+  const commentInput = commentForm.querySelector('input[type="text"]');
+  const addButton = document.getElementById('comment-add');
+  const cancelButton = document.getElementById('comment-cancel');
+  const commentList = document.querySelector('.comment-list');
+
+  addButton.addEventListener('click', function() {
+    const commentText = commentInput.value.trim();
+    
+    if (commentText) {
+      const newComment = document.createElement('li');
+      newComment.innerHTML = `
+        <div class="comment-item">
+          <div class="comment-author">
+            <img
+              src="./images/comment-author-icon.png"
+              alt="사용자 프로필 이미지"
+              class="comment-icon"
+            />
+            <span>방문자</span>
+          </div>
+          <div class="comment-content">
+            ${commentText}
+          </div>
+        </div>
+      `;
+      commentList.insertBefore(newComment, commentList.firstChild);
+      alert("댓글이 등록되었습니다");
+      commentInput.value = '';
+    }
+  });
+
+  cancelButton.addEventListener('click', function() {
+    commentInput.value = '';
+  });
+
+  commentForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+  });
+});
